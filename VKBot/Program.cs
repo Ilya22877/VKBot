83bc005e55b545f6eeeb65a379bb3bb3be26a01c
Ilya <menchinskiyia@skbkontur.ru>
2019-01-15 18:57:08 +0500

    VKnet lidrary

--- VKBot/Program.cs
+++ VKBot/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
 
 namespace VKBot
 {
@@ -10,6 +10,21 @@ namespace VKBot
 	{
 		static void Main(string[] args)
 		{
+			var api = new VkApi();
+			api.Authorize(new ApiAuthParams
+			{
+				ApplicationId = 6820217,
+				Login = "",
+				Password = "",
+				Settings = Settings.Wall
+			});
+
+			var posts = api.Wall.Get(new WallGetParams());
+			var res = api.Groups.Get(new GroupsGetParams());
+
+			Console.WriteLine(res.TotalCount);
+
+			Console.ReadLine();
 		}
 	}
 }
